4ebfbe72480bd38c7b549bb3a596d3cc1bc6acfe
Vincent <vkl2109@columbia.edu>
2023-01-30 11:19:28 -0500

    added user model

--- pollio_server/pollio/models.py
+++ pollio_server/pollio/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.charField(max_length=64)
+    password = models.charField(max_length=64)
+    avatarBase64 = models.charField()
+
+    def __str__(self):
+        return f"{self.id} - {self.username}"
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+        }
