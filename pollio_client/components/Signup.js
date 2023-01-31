e3bdf9c0dedcba0637b8bf278589466b29dcc780
Vincent <vkl2109@columbia.edu>
2023-01-31 16:19:47 -0500

    added option to add avatar image from library

--- pollio_client/components/Signup.js
+++ pollio_client/components/Signup.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect, useRef  } from 'react'
 import { StyleSheet, ScrollView, View, Text, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { TextInput } from 'react-native-paper';
-import { Button, Dialog } from '@rneui/themed';
+import { Button, Dialog, Icon } from '@rneui/themed';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import * as SecureStore from 'expo-secure-store';
 import { captureRef } from 'react-native-view-shot';
 import * as MediaLibrary from 'expo-media-library';
 import * as FileSystem from 'expo-file-system';
+import * as ImagePicker from 'expo-image-picker';
 import { Camera } from 'expo-camera';
 
 export default function Signup ({ navigation }) {
@@ -20,8 +21,10 @@ export default function Signup ({ navigation }) {
     const [ errorDialog, setErrorDialog ] = useState(false)
     const [ hasCameraPermission, setHasCameraPermission ] = useState(null);
     const [ camera, setCamera ] = useState(null);
-    const [ base64Image, setBase64Image ] = useState(null)
+    const [ base64Image, setBase64Image ] = useState('')
     const [ avatar, setAvatar ] = useState()
+    const [ chooseAvatar, setChooseAvatar ] = useState(false)
+    const [ library, setLibrary ] = useState(false)
     const [status, requestPermission] = MediaLibrary.usePermissions();
     const imageRef = useRef();
 
@@ -47,6 +50,7 @@ export default function Signup ({ navigation }) {
                 body: JSON.stringify({
                     username: username,
                     password: password,
+                    avatarBase64: base64Image,
                 })
             })
             let res = await req.json()
@@ -86,6 +90,24 @@ export default function Signup ({ navigation }) {
         }
     }
 
+    const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            const base64 = await FileSystem.readAsStringAsync(result.assets[0].uri, { encoding: 'base64' });
+            setBase64Image(base64)
+            const img = "data:image/jpeg;base64," + base64
+            setAvatar(img);
+            setChooseAvatar(true)
+            setLibrary(true)
+        }
+    };
+
     const onSaveImageAsync = async () => {
         try {
             const localUri = await captureRef(imageRef, {
@@ -101,6 +123,11 @@ export default function Signup ({ navigation }) {
             alert(e);
         }
     };
+
+    const toggleCamera = () => {
+        setChooseAvatar(true)
+        setLibrary(false)
+    }
 
     return (
         <SafeAreaView style={styles.container}>
@@ -139,60 +166,124 @@ export default function Signup ({ navigation }) {
                             secureTextEntry={hide2}
                             right={<TextInput.Icon icon="eye" onPress={() => setHide2(hide2 => !hide2)} style={{ justifyContent: 'center'}}/>}
                             />
-                        {avatar ? 
-                        <View style={styles.avatarContainer}>
-                            <View style={styles.buttonContainer}> 
-                                <Button
-                                    title={"retake"}
-                                    buttonStyle={{
-                                        backgroundColor: '#FFA500',
-                                        borderColor: 'transparent',
-                                        borderWidth: 0,
-                                        borderRadius: 30,
-                                        paddingTop: 6
-                                    }}
-                                    containerStyle={{
-                                        width: 100,
-                                        height: 40,
-                                        marginHorizontal: 10,
-                                        marginVertical: 10,
-                                    }}
-                                    titleStyle={{ fontWeight: 'bold' }}
-                                    onPress={() => setAvatar()}
-                                    />
-                                <Button
-                                    title={"save"}
-                                    buttonStyle={{
-                                        backgroundColor: '#FFA500',
-                                        borderColor: 'transparent',
-                                        borderWidth: 0,
-                                        borderRadius: 30,
-                                        paddingTop: 6
-                                    }}
-                                    containerStyle={{
-                                        width: 100,
-                                        height: 40,
-                                        marginHorizontal: 10,
-                                        marginVertical: 10,
-                                    }}
-                                    titleStyle={{ fontWeight: 'bold' }}
-                                    onPress={() => onSaveImageAsync()}
-                                    />
+                        {chooseAvatar && 
+                        <>
+                            {avatar ? 
+                            <View style={styles.avatarContainer}>
+                                <View style={styles.buttonContainer}> 
+                                    {library ? 
+                                    <Button
+                                        title={""}
+                                        buttonStyle={{
+                                            backgroundColor: '#FFA500',
+                                            borderColor: 'transparent',
+                                            borderWidth: 0,
+                                            borderRadius: 30,
+                                            paddingTop: 6,
+                                            height: 40,
+                                            width: 40
+                                        }}
+                                        containerStyle={{
+                                            width: 40,
+                                            height: 40,
+                                            marginHorizontal: 30,
+                                            marginVertical: 10,
+                                        }}
+                                        titleStyle={{ fontWeight: 'bold' }}
+                                        onPress={() => pickImage()}
+                                        icon={<Icon name="photo" size={20} color="white" />}
+                                        />
+                                    :
+                                    <Button
+                                        title={""}
+                                        buttonStyle={{
+                                            backgroundColor: '#FFA500',
+                                            borderColor: 'transparent',
+                                            borderWidth: 0,
+                                            borderRadius: 30,
+                                            paddingTop: 6,
+                                            height: 40,
+                                            width: 40
+                                        }}
+                                        containerStyle={{
+                                            width: 40,
+                                            height: 40,
+                                            marginHorizontal: 30,
+                                            marginVertical: 10,
+                                        }}
+                                        titleStyle={{ fontWeight: 'bold' }}
+                                        onPress={() => setAvatar()}
+                                        icon={<Icon name="camera-alt" size={20} color="white" />}
+                                        />}
+                                    <Button
+                                        title={""}
+                                        buttonStyle={{
+                                            backgroundColor: '#FFA500',
+                                            borderColor: 'transparent',
+                                            borderWidth: 0,
+                                            borderRadius: 30,
+                                            paddingTop: 6,
+                                            height: 40,
+                                            width: 40
+                                        }}
+                                        containerStyle={{
+                                            width: 40,
+                                            height: 40,
+                                            marginHorizontal: 30,
+                                            marginVertical: 10,
+                                        }}
+                                        titleStyle={{ fontWeight: 'bold' }}
+                                        onPress={() => onSaveImageAsync()}
+                                        icon={<Icon name="save" size={20} color="white" />}
+                                        iconRight
+                                        />
+                                </View>
+                                <View ref={imageRef} collapsable={false}>
+                                    <Image source={{ uri: avatar }} style={styles.image} />
+                                </View>
                             </View>
-                            <View ref={imageRef} collapsable={false}>
-                                <Image source={{ uri: avatar }} style={styles.image} />
-                            </View>
-                        </View>
-                        :
-                        <View style={styles.avatarContainer}>
-                            <Button
-                            title={"+"}
+                            :
+                            <View style={styles.avatarContainer}>
+                                <Button
+                                title={""}
+                                buttonStyle={{
+                                    backgroundColor: '#FFA500',
+                                    borderColor: 'transparent',
+                                    borderWidth: 0,
+                                    borderRadius: 30,
+                                    paddingTop: 6,
+                                    height: 40,
+                                    width: 40
+                                }}
+                                containerStyle={{
+                                    width: 40,
+                                    height: 40,
+                                    marginHorizontal: 50,
+                                    marginVertical: 10,
+                                }}
+                                titleStyle={{ fontWeight: 'bold' }}
+                                onPress={() => takePicture()}
+                                icon={<Icon name="camera" size={20} color="white" />}
+                                />
+                                <View style={styles.cameraContainer}>
+                                    <Camera
+                                        ref={ref => setCamera(ref)}
+                                        style={styles.fixedRatio}
+                                        type={Camera.Constants.Type.front}
+                                        ratio={'1:1'} />
+                                </View>
+                            </View>}
+                        </>}
+                        {chooseAvatar ? 
+                        <Button
+                            title={""}
                             buttonStyle={{
                                 backgroundColor: '#FFA500',
                                 borderColor: 'transparent',
                                 borderWidth: 0,
                                 borderRadius: 30,
-                                paddingTop: 6
+                                width: 40,
+                                height: 40,
                             }}
                             containerStyle={{
                                 width: 40,
@@ -200,16 +291,50 @@ export default function Signup ({ navigation }) {
                                 marginHorizontal: 50,
                                 marginVertical: 10,
                             }}
-                            titleStyle={{ fontWeight: 'bold' }}
-                            onPress={() => takePicture()}
+                            onPress={() => setChooseAvatar(false)}
+                            icon={<Icon name="arrow-circle-up" size={20} color="white" />}
                             />
-                            <View style={styles.cameraContainer}>
-                                <Camera
-                                    ref={ref => setCamera(ref)}
-                                    style={styles.fixedRatio}
-                                    type={Camera.Constants.Type.front}
-                                    ratio={'1:1'} />
-                            </View>
+                        : <View style={styles.buttonContainer}> 
+                            <Button
+                                title={""}
+                                buttonStyle={{
+                                    backgroundColor: '#FFA500',
+                                    borderColor: 'transparent',
+                                    borderWidth: 0,
+                                    borderRadius: 30,
+                                    width: 40,
+                                    height: 40,
+                                }}
+                                containerStyle={{
+                                    width: 40,
+                                    height: 40,
+                                    marginHorizontal: 20,
+                                    marginVertical: 10,
+                                }}
+                                titleStyle={{ fontWeight: 'bold' }}
+                                onPress={() => toggleCamera()}
+                                icon={<Icon name="camera-alt" size={20} color="white" />}
+                                />
+                            <Button
+                                title={""}
+                                buttonStyle={{
+                                    backgroundColor: '#FFA500',
+                                    borderColor: 'transparent',
+                                    borderWidth: 0,
+                                    borderRadius: 30,
+                                    width: 40,
+                                    height: 40,
+                                }}
+                                containerStyle={{
+                                    width: 40,
+                                    height: 40,
+                                    marginHorizontal: 20,
+                                    marginVertical: 10,
+                                }}
+                                titleStyle={{ fontWeight: 'bold' }}
+                                onPress={() => pickImage()}
+                                icon={<Icon name="photo" size={20} color="white" />}
+                                />
                         </View>}
                         <Button
                             title={"SIGN UP"}
