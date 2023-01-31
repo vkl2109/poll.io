e31877be428e9e133f7e4e16eea658c35631e035
Vincent <vkl2109@columbia.edu>
2023-01-31 17:42:24 -0500

    added styling to tab navigation in main

--- pollio_client/components/Main.js
+++ pollio_client/components/Main.js
@@ -4,16 +4,46 @@ import Feed from './Feed'
 import YourPolls from './YourPolls'
 import CreatePoll from './CreatePoll'
 import Profile from './Profile'
+import { Icon } from '@rneui/themed';
+
 
 const Tab = createBottomTabNavigator();
 
 export default function Main () {
     return (
-        <Tab.Navigator screenOptions={{ headerShown: false }}>
-            <Tab.Screen name="Feed" component={Feed} />
-            <Tab.Screen name="YourPolls" component={YourPolls} />
-            <Tab.Screen name="CreatePoll" component={CreatePoll} />
-            <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Navigator screenOptions={{ headerShown: false, tabBarActiveTintColor: '#ADD8E6', }} >
+            <Tab.Screen name="Feed" component={Feed} 
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="home" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tab.Screen name="YourPolls" component={YourPolls} 
+                options={{
+                    tabBarLabel: 'Polls',
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="poll" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tab.Screen name="CreatePoll" component={CreatePoll}
+                options={{
+                    tabBarLabel: 'Create',
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="add-circle-outline" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tab.Screen name="Profile" component={Profile}
+                options={{
+                    tabBarLabel: 'Profile',
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="account-circle" color={color} size={size} />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     )
 }
