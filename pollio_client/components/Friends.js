43f7c38bca67f22006efc0aeab7e6380d861e240
Vincent <vkl2109@columbia.edu>
2023-02-05 16:34:52 -0500

    added basic styling to friends and poll pages

--- pollio_client/components/Friends.js
+++ pollio_client/components/Friends.js
@@ -1,9 +1,26 @@
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, ScrollView, Text, View, Dimensions } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+const screenWidth = Dimensions.get('window').width; 
 
 export default function Friends () {
     return (
-        <View>
-            <Text>Friends</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView contentContainerStyle={{flexGrow: 1, justifyContent: 'center', alignItems: 'center', width: screenWidth}}>
+                <View>
+                    <Text>Friends</Text>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexGrow: 1,
+    backgroundColor: '#ADD8E6', // '#25292e'
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
